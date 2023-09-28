e395c8f54810a5aa64a0034f96ab4c6c830ef82c
Yunn\user <tianhsin0716@gmail.com>
2023-09-28 23:21:51 +0800

    final

--- formTest.php
+++ formTest.php
@@ -10,26 +10,103 @@
 <p>
   <input type="submit" name="submit" id="submit" value="送出">
   </br>
-<?php
+  <?php
+	session_start();
+	date_default_timezone_set('Asia/taipei');
 	
-		session_start();
-		
-		if(!isset($_SESSION['submit']))
-			$arr=array();
+	$conn = mysqli_connect('localhost', 'root','','107php');
 
-	
-		while (count($arr)<3)
-		{
-			$ran = rand(0,9);
-
-			if(!in_array($ran,$arr))
-			{
-				$arr[]=$ran;
+	if(isset($_POST["submit"])){
+			$arr = array();
+//隨機產生3數
+			while(count($arr) < 3){
+				$ran = rand(0,9);
+				if(!in_array($ran,$arr)){
+					$arr[] = $ran;
+				}
 			}
-		}
-		print_r($arr);
-			echo "</br>";
 			
-		
+			$_SESSION['arr'] = $arr;
+			
+			if(!isset($_SESSION['detail'])){
+				$_SESSION['detail'] = "";
+				$_SESSION['detail'] .= implode("",$arr).",";
+			}
+			else{
+				$_SESSION['detail'] .= implode("",$arr).",";
+			}
+
+			if(!isset($_SESSION['count'])){
+				$_SESSION['count'] = 1;
+			}
+			else{
+				$_SESSION['count'] += 1;
+			} //計算次數
+
+			if(!isset($_SESSION['ans'])){
+				$_SESSION['ans'] = "";
+			}
+
+			if(!isset($_SESSION['check'])){
+				$_SESSION['check'] = 0; //按第一次
+			}
+			else{
+				$_SESSION['check'] = 1; 
+			}
+			
+			$result = abs($_SESSION['arr'][1] - $_SESSION['arr'][0]) ==  abs($_SESSION['arr'][2] - $_SESSION['arr'][1]); 
+
+			$_SESSION['ans'] .= $_SESSION['count']." = ";
+			for($i=0;$i<count($_SESSION['arr']);$i++){
+				$_SESSION['ans'] .= $_SESSION['arr'][$i].",";
+			}
+			$_SESSION['ans'] .= "</br>";
+			
+			if($_SESSION['count'] <= 9){
+				if($result){
+					$_SESSION['ans'] .= "總共試了".$_SESSION['count']."次或成功";
+					$today = date("YmdHis");
+					$sql = "INSERT INTO `mymaster`(`id`, `freq`) VALUES ('$today','".$_SESSION['count']."')";
+					mysqli_query($conn,$sql);
+					
+					$detail = explode(",",$_SESSION['detail']);
+					for($i=0;$i<count($detail)-1;$i++){
+						$sql = "INSERT INTO `mydetail`(`id`, `turn`, `rec`) VALUES ('$today','".($i+1)."','".$detail[$i]."')";
+						mysqli_query($conn,$sql);
+					}
+					session_destroy();
+				}
+			}
+			else{
+				$_SESSION['ans'] .= "總共試了".$_SESSION['count']."次或成功";
+
+				$today = date("YmdHis");
+				$sql = "INSERT INTO `mymaster`(`id`, `freq`) VALUES ('$today','".$_SESSION['count']."')";
+				mysqli_query($conn,$sql);
+				
+				$detail = explode(",",$_SESSION['detail']);
+				for($i=0;$i<count($detail)-1;$i++){
+					$sql = "INSERT INTO `mydetail`(`id`, `turn`, `rec`) VALUES('$today','".($i+1)."','".$detail[$i]."')";
+					mysqli_query($conn,$sql);
+				}
+				echo $_SESSION['detail'];
+				echo "</br>";
+				print_r( $detail);
+				echo "</br>";
+				mysqli_close($conn);
+				session_destroy();
+			}		
+	}
+	
+	if(isset($_SESSION['check'])){
+		if($_SESSION['check'] == 1){
+			print_r($_SESSION['ans']);
+		}
+		else{
+			print_r($_SESSION['arr']);
+			echo "</br>";
+		}
+	}
+	//session_destroy();
 
 ?>
